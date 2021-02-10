d0d2de3842c7e39d52faccc89f306ec1a7737f55
Nayra <nayradb@gmail.com>
2021-02-10 15:45:08 +0100

    version clase

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>Prueba de PHP</title>
  </head>
  <body>
- <?php echo '<p>Prueba con PHP de pipelines CICD con Azure DevOps</p>'; ?>
+ <?php echo '<p>Prueba con PHP de pipelines CICD con Azure DevOps testeo del miercoles 10 febrero</p>'; ?>
  </body>
 </html>
